6d8a0ea74882b1230f1301c9fa5aa5a5ca16902b
Gulnaz <666lynx666@mail.ru>
2018-04-18 21:58:49 +0300

    Initial commit

---
+++ Dices/Dices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dices
+//
+//  Created by lynx on 18/04/2018.
+//  Copyright © 2018 Gulnaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
