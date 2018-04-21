ef55c241d0752eaf70e1eb38948c8acadef271b2
Gulnaz <666lynx666@mail.ru>
2018-04-21 14:48:13 +0300

    add magic 8 ball app

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by lynx on 21/04/2018.
+//  Copyright © 2018 Gulnaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
