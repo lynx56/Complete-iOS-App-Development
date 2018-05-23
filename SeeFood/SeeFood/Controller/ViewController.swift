6699b7e76996bb1c5effa816a8df63d813ef9223
Gulnaz <666lynx666@mail.ru>
2018-05-23 15:27:14 +0300

    SeeFood. Colorify navBar and add sharing

--- SeeFood/SeeFood/Controller/ViewController.swift
+++ SeeFood/SeeFood/Controller/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 import Vision
+import Social
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
+    @IBOutlet weak var hotdogThumbnail: UIImageView!
     var pickerViewController: UIImagePickerController!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var libraryButton: UIBarButtonItem!
     
   //  var master: ImageRecognitionMasterProtocol = BlumixRecognitionMaster()
     var master: ImageRecognitionMasterProtocol = ImageRecognitionMaster()
@@ -26,6 +31,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         self.pickerViewController.delegate = self
         self.pickerViewController.allowsEditing = false
+        self.shareButton.isHidden = true
+        self.setActivityNavigationBarButtons(isEnabled: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -41,12 +48,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(pickerViewController, animated: true, completion: nil)
     }
     
+    @IBAction func share(_ sender: Any) {
+        let text = "My food is \(navigationItem.title!)"
+        let image = imageView.image!
+        
+        let activityViewController = UIActivityViewController(activityItems: [text, image], applicationActivities: nil)
+        
+        self.popoverPresentationController?.sourceView = sender as? UIView
+        
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func  setActivityNavigationBarButtons(isEnabled: Bool){
+        self.cameraButton.isEnabled = isEnabled
+        self.libraryButton.isEnabled = isEnabled
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
             DispatchQueue.main.async {
                 self.imageView.image = image
                 self.activityIndicator.startAnimating()
             }
+            setActivityNavigationBarButtons(isEnabled: false)
             master.recognize(image: image, completion: { (results, error) in
                 DispatchQueue.main.async {
                     self.activityIndicator.stopAnimating()
@@ -54,12 +78,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                  
                     if array.contains(where: {$0.id.contains("hotdog")}){
                         self.navigationItem.title = "Hot Dog!"
-                        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.blue]
+                        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.2078431373, green: 0.9843137255, blue: 0.1529411765, alpha: 1)
+                        self.hotdogThumbnail.image = #imageLiteral(resourceName: "hotdog")
+                        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.yellow, NSAttributedStringKey.font : UIFont.systemFont(ofSize: 30)]
                     }else{
                         self.navigationItem.title = "Not Hot Dog!"
-                        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.red]
+                        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.9098039216, green: 0.2666666667, blue: 0.2352941176, alpha: 1)
+                        self.hotdogThumbnail.image = #imageLiteral(resourceName: "not-hotdog")
+                        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.yellow, NSAttributedStringKey.font : UIFont.systemFont(ofSize: 30)]
                         
                     }
+                    
+                     self.shareButton.isHidden = false
+                     self.setActivityNavigationBarButtons(isEnabled: true)
                 }
             })
         }
